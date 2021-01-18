1f25de2e93a09b884b4068ee66194e57dfa91b05
Rohit Kumar <rohitdotnet@outlook.com>
2021-01-18 09:12:23 +0530

    initial commit for comments

--- force-app/main/default/lwc/demo/demo.js
+++ force-app/main/default/lwc/demo/demo.js
@@ -1,3 +1,13 @@
-import { LightningElement } from 'lwc';
+/*
+ * Author:- Rohit Kumar
+ * Created By:-
+ * Created Date:-
+ * Last Modified By:-
+ * Last Modified Date:-
+ * Description:-
+ * Version    Date       Author          Modification
+ *  1.0                  Rohit Kumar     Initial
+ */
+import { LightningElement } from "lwc";
 
 export default class Demo extends LightningElement {}
